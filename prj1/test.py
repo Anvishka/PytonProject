e6f8dbb159f254323fcfacaaafe54c7fe1b7009a
Аудитория 7 <aud7_st@step.local>
2024-03-10 16:13:45 +0200

    IsAdultCheck

--- prj1/test.py
+++ prj1/test.py
@@ -1,1 +1,12 @@
 print("Hello")
+
+def check_adulthood(year_of_birth):
+    current_year = 2024
+    age = current_year - year_of_birth
+    if age >= 18:
+         print("Ви повнолітній.")
+    else:
+         print("Ви не повнолітній.")
+
+year_of_birth = int(input("Ведіть ваш вік: "))
+check_adulthood(year_of_birth)
